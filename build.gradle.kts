9db07c00a3caff5ebe618a93600de599086eb4aa
Takexito <Takexito@Gmail.com>
2022-06-03 11:40:54 +0300

    init

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+buildscript {
+
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
+    }
+}
