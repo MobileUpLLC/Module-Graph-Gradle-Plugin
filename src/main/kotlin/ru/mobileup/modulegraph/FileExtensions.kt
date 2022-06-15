7928841df08f470b15bdfeca063764ef06c1d445
Takexito <Takexito@Gmail.com>
2022-06-15 14:58:30 +0300

    clean up FileExtensions.kt

--- src/main/kotlin/ru/mobileup/modulegraph/FileExtensions.kt
+++ src/main/kotlin/ru/mobileup/modulegraph/FileExtensions.kt
@@ -1,6 +1,5 @@
 package ru.mobileup.modulegraph
 
-import org.gradle.api.Project
 import java.io.File
 import java.nio.file.Files
 import java.nio.file.Path
@@ -11,21 +10,16 @@ fun Path.createPathIfNotExist() {
     }
 }
 
-fun String.getFileFromProjectRelativePath(project: Project): File {
-    return File(project.projectDir.path + "/" + this)
-}
-
 fun File.processFilesFromFolder(actionOnFile: (file: File) -> Boolean): Boolean {
     val folderEntries = listFiles()
     var interrupt = false
-    var hasImports: Boolean
     folderEntries?.forEach { entry ->
         when {
             interrupt -> return true
             entry.isDirectory -> interrupt = entry.processFilesFromFolder(actionOnFile)
             else -> {
-                hasImports = actionOnFile.invoke(entry)
-                if (hasImports) return true
+                val actionInterrupt = actionOnFile.invoke(entry)
+                if (actionInterrupt) return true
             }
         }
     }
